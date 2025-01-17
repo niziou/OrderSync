81636055de2a7084135ff60fa63abe8274cdaf6b
Mateusz Niziolek <mateusz.niziolek@gmail.com>
2025-01-17 20:21:22 +0100

    [Auto1] Task 1 User Class

---
+++ Auto1/User.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+class User
+{
+    protected string $firstName;
+    protected string $lastName;
+    protected string $email;
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName(string $firstName): User
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName(string $lastName): User
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+        return $this;
+    }
+}
